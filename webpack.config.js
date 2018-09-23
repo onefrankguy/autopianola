1b71b348ac378b87b5c1170b8b406dbab90a3d0a
Frank Mitchell <me@frankmitchell.org>
2018-09-23 11:08:43 -0400

    Set up a CSS build process.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,24 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
   mode: 'production',
   entry: './src/autopianola.js',
   output: {
     filename: 'autopianola.js'
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'autopianola.css'
+    })
+  ],
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        'css-loader',
+        'sass-loader'
+      ]
+    }]
   }
 };
