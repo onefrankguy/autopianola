fdaf35ebe760715e89bcbfe97895fe7408000788
Frank Mitchell <me@frankmitchell.org>
2018-09-23 11:56:51 -0400

    Wrap UI interaction up in my tiny jQuery clone.

--- src/autopianola.js
+++ src/autopianola.js
@@ -1,9 +1,11 @@
 import './autopianola.scss';
 
-window.onload = function () {
-  const play = document.getElementById('play');
-  play.onclick = function () {
-    play.classList.toggle('play');
-    play.classList.toggle('pause');
-  };
+import './jquery.js';
+
+window.onload = () => {
+  const $ = window.jQuery;
+  $('#play').click((e) => {
+    e.toggleClass('play');
+    e.toggleClass('pause');
+  });
 };
---
+++ src/jquery.js
@@ -0,0 +1,73 @@
+(function $() {
+  function Fn(selector) {
+    if (selector instanceof Fn) {
+      return selector;
+    }
+
+    this.element = selector;
+
+    if (typeof selector === 'string') {
+      if (selector.indexOf('#') === 0) {
+        this.element = document.getElementById(selector.slice(1));
+      }
+    }
+
+    return this;
+  }
+
+  Fn.prototype.toggleClass = function toggleClass(klass) {
+    if (this.element && this.element.classList) {
+      this.element.classList.toggle(klass);
+    }
+
+    return this;
+  };
+
+  Fn.prototype.click = function click(start, end) {
+    const that = this;
+
+    if (this.element) {
+      if ('ontouchstart' in document.documentElement === false) {
+        this.element.onmousedown = function onmousedown(mouseDownEvent) {
+          if (start) {
+            start(that, mouseDownEvent);
+          }
+          document.onmousemove = function onmousemove(e) {
+            e.preventDefault();
+          };
+          document.onmouseup = function onmouseup(e) {
+            if (end) {
+              end(that, e);
+            }
+            document.onmousemove = undefined;
+            document.onmouseup = undefined;
+          };
+        };
+      } else {
+        this.element.ontouchstart = function ontouchstart(touchStartEvent) {
+          if (start) {
+            start(that, touchStartEvent);
+          }
+          document.ontouchmove = function ontouchmove(e) {
+            e.preventDefault();
+          };
+          document.ontouchend = function ontouchend(e) {
+            if (end) {
+              end(that, e);
+            }
+            document.ontouchmove = undefined;
+            document.ontouchend = undefined;
+          };
+        };
+      }
+    }
+
+    return that;
+  };
+
+  function root(selector) {
+    return new Fn(selector);
+  }
+
+  window.jQuery = root;
+}());
